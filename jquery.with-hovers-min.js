0edf7b8950fa938750388192ea303d6aa6ed67e9
Daniel Cohen Gindi <danielgindi@gmail.com>
2015-12-29 19:00:07 +0200

    Support for combined touch+mouse dynamically

--- jquery.with-hovers-min.js
+++ jquery.with-hovers-min.js
@@ -1,5 +1,7 @@
 /*
-    Library by Daniel Cohen Gindi (danielgindi@gmail.com) 054-5655765 
+    Written by Daniel Cohen Gindi (danielgindi@gmail.com)
+	https://github.com/danielgindi/jquery.with-hovers
+	v1.1.0
     MIT License!
 */
-(function(a){var b=null,c=function(){(null==b||400<+new Date-b)&&a(document.documentElement).addClass("with-hovers");a(window).off("mousemove",c).off("touchend",d)},d=function(){b=+new Date;setTimeout(function(){a(window).off("mousemove",c).off("touchend",d)},400)};a(window).on("mousemove",c).on("touchend",d)})(jQuery);
+(function(a){function c(){if(null==b||400<+new Date-b)a(document.documentElement).addClass("with-hovers"),a(window).off("mousemove",c).off("touchend",d)}function d(){b=+new Date}var b=null;a(document.documentElement).addClass("with-hovers");a(window).on("touchstart",function(){a(document.documentElement).removeClass("with-hovers");a(window).on("mousemove",c).on("touchend",d)})})(jQuery);
--- jquery.with-hovers.js
+++ jquery.with-hovers.js
@@ -1,29 +1,56 @@
 // ==ClosureCompiler==
 // @compilation_level ADVANCED_OPTIMIZATIONS
-// @externs_url https://closure-compiler.googlecode.com/git/contrib/externs/jquery-1.9.js
+// @externs_url https://raw.githubusercontent.com/google/closure-compiler/master/contrib/externs/jquery-1.9.js
 // ==/ClosureCompiler==
-/** @preserve    Library by Daniel Cohen Gindi (danielgindi@gmail.com) 054-5655765 
+/** @preserve    Written by Daniel Cohen Gindi (danielgindi@gmail.com)
+	https://github.com/danielgindi/jquery.with-hovers
+	v1.1.0
     MIT License!
 */
 (function($){
+
+    var touchDetected = null,
+        timeout = null;
+    
+    var touchStartHook = function () {
         
-    var touchDetected = null;
+        // We have touch. Disable hovers until we detect a legitimate mouse.
+        $(document.documentElement)
+            .removeClass('with-hovers');
+           
+        $(window)
+            .on('mousemove', mouseHook)
+            .on('touchend', touchEndHook);
+    };
+    
+    var touchEndHook = function () {
+        touchDetected = +new Date();
+    };
+    
     var mouseHook = function () {
         if (touchDetected == null || +new Date() - touchDetected > 400) {
-            // We may have a touch screen, but there's a mouse for sure, as it was not simulated after a touchend.
-            $(document.documentElement).addClass('with-hovers');
+            
+            // We may have a touch screen, but there's a mouse for sure,
+            //   as it was not simulated after a touchend.
+            $(document.documentElement)
+                .addClass('with-hovers');
+        
+            $(window)
+                .off('mousemove', mouseHook)
+                .off('touchend', touchEndHook);
+            
         } else {
-            // We have a touch screen, without a mouse. Mouse events are simulated!
+            // Mouse events are simulated!
+            // So we have a touch screen, without a mouse.
         }
-        $(window).off('mousemove', mouseHook).off('touchend', touchHook);
-    }, touchHook = function () {
-        touchDetected = +new Date();
-
-        setTimeout(function(){
-            // Nothing happened by now, stop looking for it...
-            $(window).off('mousemove', mouseHook).off('touchend', touchHook);
-        }, 400);
     };
-    $(window).on('mousemove', mouseHook).on('touchend', touchHook);
+    
+    // Start with hovers
+    $(document.documentElement)
+        .addClass('with-hovers');
+    
+    // Listen for touch start
+    $(window)
+        .on('touchstart', touchStartHook);
 
 })(jQuery);
